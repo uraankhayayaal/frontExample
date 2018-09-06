96f544fb777541df88ac5eb4a0764d86eb684c77
uraankhayayaal <uraankhayayaal@gmail.com>
2018-09-07 01:05:35 +0900

    fix

--- js/run.js
+++ js/run.js
@@ -6,4 +6,16 @@ $(function() {
 	    items:1,
 	    dots:false,
 	});
+	$(window).scroll(function (event) {
+	    var scroll = $(window).scrollTop();
+	    if(scroll > 10){
+	    	$( "body" ).addClass("inverse");
+	    }else{
+	    	$( "body" ).removeClass("inverse");
+	    }
+	});
 });
+
+function clickBurger(){
+	$( ".menuMobile" ).slideToggle();
+}
